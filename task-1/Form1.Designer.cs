f0b58f9d47e19e65f2721a25074bef81640dbc22
Анна Рязанова <ryazanoanya@yandex.ru>
2017-09-27 13:17:28 +0300

    Start task1

---
+++ task-1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace task_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paletteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 225);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImageToolStripMenuItem,
+            this.paletteToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(288, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(150, 20);
+            this.loadImageToolStripMenuItem.Text = "Загрузить изображение";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // paletteToolStripMenuItem
+            // 
+            this.paletteToolStripMenuItem.Name = "paletteToolStripMenuItem";
+            this.paletteToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.paletteToolStripMenuItem.Text = "Палитра";
+            this.paletteToolStripMenuItem.Click += new System.EventHandler(this.paletteToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(288, 249);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Task1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paletteToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ task-1/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_1
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private Bitmap image;
+        private Color paletteColor;
+        private Color curPixel;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+            paletteColor = Color.White;
+        }
+        
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image Files(*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG)|*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG|All files (*.*)|*.*"; //формат загружаемого файла
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    image = new Bitmap(openDialog.FileName);
+                    pictureBox1.Image = image;
+                    pictureBox1.Invalidate();
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void paletteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                paletteColor = colorDialog1.Color;
+            }
+        }
+
+
+        private void paint(int x, int y, Color curColor, Color newColor)
+        {
+            if (curColor == newColor)
+                return;
+            if (image.GetPixel(x, y) != curColor)
+                return;
+
+            image.SetPixel(x, y, newColor);
+
+            /* Исправить
+            if (y >= 0 && y < image.Height)
+            {
+                
+                    int leftX, rightX;
+
+                    if (x == 0)
+                        leftX = 0;
+                    else
+                        leftX = x - 1;
+
+                    if (x == (image.Width - 1))
+                        rightX = (image.Width - 1);
+                    else
+                        rightX = x + 1;
+
+                    var pix = image.GetPixel(leftX, y);
+                    while (leftX > 0 && pix == curColor)
+                    {
+                        leftX--;
+                        pix = image.GetPixel(leftX, y);
+                    }
+                    if (leftX == 0 && image.GetPixel(0, y) == curColor)
+                        leftX = 0;
+
+                    pix = image.GetPixel(rightX, y);
+                    while (rightX < (image.Width - 1) && pix == curColor)
+                    {
+                        rightX++;
+                        pix = image.GetPixel(rightX, y);
+                    }
+                    if (rightX == (image.Width - 1) && image.GetPixel(0, y) == curColor)
+                        rightX = (image.Width - 1);
+
+                    Pen pen = new Pen(newColor);
+                    g.DrawLine(pen, leftX, y, rightX, y);
+
+                    for (int i = leftX; i <= rightX; ++i)
+                    {
+                        var newY = y + 1;
+                        pix = image.GetPixel(i, newY);
+                        if (pix == curColor)
+                            paint(i, newY, curColor, newColor);
+
+                        newY = y - 1;
+                        pix = image.GetPixel(i, newY);
+                        if (pix == curColor)
+                            paint(i, newY, curColor, newColor);
+                    }
+            }
+            */
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                var x = e.Location.X;
+                var y = e.Location.Y;
+                curPixel = (pictureBox1.Image as Bitmap).GetPixel(e.Location.X, e.Location.Y);
+                paint(x, y, curPixel, paletteColor);
+                pictureBox1.Image = image;
+            }
+        }
+    }
+}
